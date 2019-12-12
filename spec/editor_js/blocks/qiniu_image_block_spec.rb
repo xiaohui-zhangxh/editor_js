05038a258b4d24f16880efa201a798924b4208f5
陈成 <cheng@tanmer.com>
2019-12-13 00:21:48 +0800

    添加blocks的测试文件

---
+++ spec/editor_js/blocks/qiniu_image_block_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RSpec.describe EditorJs::Blocks::EmbedBlock do
+  let(:valid_data1) do
+    {
+      type: 'qiniu_image',
+      data: {
+        url: 'http://assets.wedxt.com/1576156689709-%E5%B1%8F%E5%B9%95%E5%BF%AB%E7%85%A7%202019-10-15%2021.34.57.png',
+        caption: 'asdasdf sadfsadf &lt;asdf &gt;&lt;fff&gt;&gt;&gt; &lt;div&gt;asdf . fasf &lt;/div&gt;',
+        withBorder: true,
+        withBackground: false,
+        stretched: false
+      }
+    }
+  end
+
+  context 'with valid string' do
+    let(:embed) { described_class.new(valid_data1.to_json) }
+
+    it { expect(embed).to be_valid }
+    it { expect(embed.render).to eq(%|<div class="editor_js--embed"><iframe src="http:/xxx" width="100%" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe><span>Hello &lt;b&gt;Movie&lt;/b&gt;</span></div>|) }
+    it { expect(embed.plain).to eq('Hello Movie') }
+  end
+
+  context 'with valid hash' do
+    let(:embed) { described_class.new(valid_data1) }
+
+    it { expect(embed).to be_valid }
+  end
+end
