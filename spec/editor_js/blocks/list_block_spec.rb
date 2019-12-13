dce38f8510f79d1ed88884a2fe20951d6294c0ab
陈成 <cheng@tanmer.com>
2019-12-13 17:35:11 +0800

    edit

--- spec/editor_js/blocks/list_block_spec.rb
+++ spec/editor_js/blocks/list_block_spec.rb
@@ -7,9 +7,23 @@ RSpec.describe EditorJs::Blocks::ListBlock do
       data: {
         style: 'ordered',
         items: [
-          'item <b>hacker</b> &lt;1&gt;',
+          "item <b>hacker</b> &lt;1&gt;<strong>大字体</strong>《没》<i>斜体<i><a href='www.baidu.com' class='red' data-href='nodata'>go baidu<a>",
           'item &lt;2&gt;',
           'item 3'
+        ]
+      }
+    }
+  end
+
+  let(:valid_data2) do
+    {
+      type: 'list',
+      data: {
+        style: 'unordered',
+        items: [
+          "列表2 <b>hacker</b> &lt;1&gt;<strong>大字体</strong>《没》<i>斜体<i><a href='www.baidu.com' class='red' data-href='nodata'>go baidu<a>",
+          '列表2 &lt;2&gt;《<html>body<html>',
+          '列表2 3'
         ]
       }
     }
@@ -19,8 +33,16 @@ RSpec.describe EditorJs::Blocks::ListBlock do
     let(:list) { described_class.new(valid_data1.to_json) }
 
     it { expect(list).to be_valid }
-    it { expect(list.render).to eq(%|<ol class="editor_js--list"><li>item  &lt;1&gt;</li><li>item &lt;2&gt;</li><li>item 3</li></ol>|) }
-    it { expect(list.plain).to eq('item <1>, item <2>, item 3') }
+    it { expect(list.render).to eq(%|<ol class=\"editor_js--list\"><li>item <b>hacker</b> &lt;1&gt;《没》<i>斜体<i><a href=\"www.baidu.com\">go baidu</a><a></a></i></i></li><li>item &lt;2&gt;</li><li>item 3</li></ol>|) }
+    it { expect(list.plain).to eq('item hacker <1>《没》斜体go baidu, item <2>, item 3') }
+  end
+
+  context 'with valid string' do
+    let(:list) { described_class.new(valid_data2.to_json) }
+
+    it { expect(list).to be_valid }
+    it { expect(list.render).to eq(%|<ul class=\"editor_js--list\"><li>列表2 <b>hacker</b> &lt;1&gt;《没》<i>斜体<i><a href=\"www.baidu.com\">go baidu</a><a></a></i></i></li><li>列表2 &lt;2&gt;《body</li><li>列表2 3</li></ul>|) }
+    it { expect(list.plain).to eq('列表2 hacker <1>《没》斜体go baidu, 列表2 <2>《body, 列表2 3') }
   end
 
   context 'with valid hash' do
--- spec/editor_js/blocks/quote_block_spec.rb
+++ spec/editor_js/blocks/quote_block_spec.rb
@@ -5,29 +5,19 @@ RSpec.describe EditorJs::Blocks::QuoteBlock do
     {
       type: 'quote',
       data: {
-        text: 'This is a quote with <b>HTML</b> as &lt;b&gt;HTML&lt;/b&gt;',
+        text: 'This is a quote with <b>HTML</b> as &lt;b&gt;HTML&lt;/b&gt;<small>notext</small>',
         caption: 'LiBai',
         alignment: 'left'
       }
     }
   end
-
-  let(:valid_data2) do
-    {
-      type: 'quote',
-      data: {
-        text: 'This is a quote align to right',
-        caption: 'LiBai',
-        alignment: 'right'
-      }
-    }
-  end
+  # alignment 暂不使用，不用测试
 
   context 'with valid string1' do
     let(:quote) { described_class.new(valid_data1.to_json) }
 
     it { expect(quote).to be_valid }
-    it { expect(quote.render).to eq(%|<div class="editor_js--quote"><span class="editor_js--quote__text">This is a quote with HTML as &lt;b&gt;HTML&lt;/b&gt;</span><span class="editor_js--quote__caption">LiBai</span></div>|) }
+    it { expect(quote.render).to eq(%|<div class="editor_js--quote"><div class="editor_js--quote__text">This is a quote with <b>HTML</b> as &lt;b&gt;HTML&lt;/b&gt;</div><div class="editor_js--quote__caption">LiBai</div></div>|) }
     it { expect(quote.plain).to eq('This is a quote with HTML as <b>HTML</b>, LiBai') }
   end
 
@@ -37,11 +27,4 @@ RSpec.describe EditorJs::Blocks::QuoteBlock do
     it { expect(quote).to be_valid }
   end
 
-  context 'align to right' do
-    let(:quote) { described_class.new(valid_data2.to_json) }
-
-    it { expect(quote).to be_valid }
-    it { expect(quote.render).to eq(%|<div class="editor_js--quote editor_js--quote--right"><span class="editor_js--quote__text">This is a quote align to right</span><span class="editor_js--quote__caption">LiBai</span></div>|) }
-    it { expect(quote.plain).to eq('This is a quote align to right, LiBai') }
-  end
 end
