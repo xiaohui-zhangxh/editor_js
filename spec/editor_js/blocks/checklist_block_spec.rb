3065dac79705ef7109280a6e318f52490885aeba
陈成 <cheng@tanmer.com>
2020-06-03 11:34:06 +0800

    更新测试checklist_block_spec文件

--- spec/editor_js/blocks/checklist_block_spec.rb
+++ spec/editor_js/blocks/checklist_block_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe EditorJs::Blocks::ChecklistBlock do
     let(:checklist) { described_class.new(valid_data1) }
 
     it { expect(checklist).to be_valid }
-    it { expect(checklist.render).to eq('<div class="editor_js--checklist"><div class="editor_js--checklist__warrper"><input type="checkbox" disabled="disabled" checked="checked"></input><label>item 1 &amp;lt;b&amp;gt;bold&amp;lt;/b&amp;gt;</label></div><div class="editor_js--checklist__warrper"><input type="checkbox" disabled="disabled"></input><label>item</label></div><div class="editor_js--checklist__warrper"><input type="checkbox" disabled="disabled"></input><label>item 3</label></div></div>') }
-    it { expect(checklist.plain).to eq('item 1 <b>bold</b>, item, item 3') }
+    it { expect(checklist.render).to eq('<div class="editor_js--checklist"><div class="editor_js--checklist__warrper"><input type="checkbox" disabled="disabled" checked="checked"></input><label>item 1 &lt;b&gt;bold&lt;/b&gt;</label></div><div class="editor_js--checklist__warrper"><input type="checkbox" disabled="disabled"></input><label>item <b>2</b></label></div><div class="editor_js--checklist__warrper"><input type="checkbox" disabled="disabled"></input><label>item 3 </label></div></div>') }
+    it { expect(checklist.plain).to eq('item 1 <b>bold</b>, item <b>2</b>, item 3')}
   end
 end
