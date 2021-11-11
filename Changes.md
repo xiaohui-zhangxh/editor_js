2fed69f824fe0ef8813ebdfa1b56acf7072c1467
张小辉 <xiaohui@tanmer.com>
2021-11-11 19:11:02 +0800

    release 0.4.0

---
+++ Changes.md
@@ -0,0 +1,6 @@
+# EditorJS Changes
+
+## 0.4.0
+
+- only discard invalid blocks instead of marking invalid to the whole document.
+- support customized invalid block renderer
--- spec/editor_js/document_spec.rb
+++ spec/editor_js/document_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe EditorJs::Document do
   let(:document_1_output) { JSON.parse(IO.read(File.expand_path('../data/document_1.output', __dir__))) }
   let(:document_2_json) { IO.read(File.expand_path('../data/document_2.json', __dir__)) }
   let(:document_3_json) { IO.read(File.expand_path('../data/document_3.json', __dir__)) }
+  let(:document_with_invalid_block_json) { IO.read(File.expand_path('../data/document_with_invalid_block.json', __dir__)) }
+  let(:document_with_invalid_block_html) { IO.read(File.expand_path('../data/document_with_invalid_block.html', __dir__)).strip }
+  let(:document_with_invalid_block_render_html) { IO.read(File.expand_path('../data/document_with_invalid_block_render.html', __dir__)).strip }
 
   let(:document_1) { described_class.new(document_1_json) }
   let(:document_2) { described_class.new(document_2_json) }
@@ -19,4 +22,30 @@ RSpec.describe EditorJs::Document do
   it('should render text as expected') { expect(document_1.plain).to eq document_1_txt }
   it { expect(document_1.output).to match(document_1_output) }
 
+  describe 'with invalid block' do
+    it('one invalid block without renderer') do
+      doc = described_class.new(document_with_invalid_block_json)
+      expect(doc.render).to eq document_with_invalid_block_html
+    end
+    it('one invalid block with renderer') do
+      render = Class.new do
+        def initialize(raw)
+          @raw = raw
+        end
+
+        def valid?
+          false
+        end
+
+        def render
+          '<div>invalid block</div>'
+        end
+
+        def plain; end
+        def output; {} end
+      end
+      doc = described_class.new(document_with_invalid_block_json, invalid_block_renderer: render)
+      expect(doc.render).to eq document_with_invalid_block_render_html
+    end
+  end
 end
